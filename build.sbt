80c66e07b9ddb9231a98a65a89b281b6b34105a8
Cheng Kuan <ck@codeck.org>
2013-10-17 01:02:37 +0800

    force java1.6 codegen for sae & unique version setting

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+version in ThisBuild := "0.3"
+
 scalaVersion in ThisBuild := "2.10.2"
 
 publishTo in ThisBuild := Some(Resolver.file("dest-repository", file("target/local-repo"))(Resolver.ivyStylePatterns))
@@ -5,4 +7,6 @@ publishTo in ThisBuild := Some(Resolver.file("dest-repository", file("target/loc
 publishMavenStyle in ThisBuild := false
 
 organization in ThisBuild := "org.codeck.play2sae"
+
+javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6")
 
--- plugin/src/main/scala/main.scala
+++ plugin/src/main/scala/main.scala
@@ -9,18 +9,19 @@ import com.earldouglas.xsbtwebplugin.PluginKeys.warPostProcess
 
 object sbtPlugin extends Plugin
 {
-    lazy val saeSettings = Seq(
-	  libraryDependencies += "org.codeck.play2sae" %% "play2-wrapper" % "0.2" excludeAll (
-		ExclusionRule(organization = "com.typesafe.play"))
-    ) ++ WarPlugin.warSettings ++ Seq(
-	  warPostProcess in Compile <<= (target) map {
-		(target) => { 
-		  () =>
-		  val webapp = target / "webapp"
-		  IO.delete(webapp / "WEB-INF" / "lib" /"servlet-api-2.5.jar")
-		}
-	  }
-	)
+  def wrapperVer = play2sae.BuildInfo.version
+
+  lazy val saeSettings = Seq(
+	libraryDependencies += "org.codeck.play2sae" %% "play2-wrapper" % wrapperVer excludeAll (
+	  ExclusionRule(organization = "com.typesafe.play"))
+  ) ++ WarPlugin.warSettings ++ Seq(
+    javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6"),
+	warPostProcess in Compile := {
+	  () =>
+	  val webapp = target.value / "webapp"
+	  IO.delete(webapp / "WEB-INF" / "lib" /"servlet-api-2.5.jar")
+	}
+  )
 }
 
 
