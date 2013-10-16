75ca42b6dfdf29cbef6ee8b8dc4a4b82af0a720b
Cheng Kuan <ck@codeck.org>
2013-10-17 01:59:45 +0800

    generate web.xml on demand

--- plugin/src/main/scala/main.scala
+++ plugin/src/main/scala/main.scala
@@ -42,6 +42,32 @@ object sbtPlugin extends Plugin
 	}
   }
 
+  def warMassage(appdir: File) {
+	IO.delete(appdir / "WEB-INF" / "lib" /"servlet-api-2.5.jar")
+	val webxml = appdir / "WEB-INF" / "web.xml"
+	if (!webxml.exists()) {
+	  IO.append(webxml, """<?xml version="1.0" ?>
+<web-app xmlns="http://java.sun.com/xml/ns/javaee"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd"
+        version="2.5">
+  <display-name>APP by play2sae</display-name>
+  <listener>
+      <listener-class>play.core.server.servlet25.Play2Servlet</listener-class>
+  </listener>
+  <servlet>
+    <servlet-name>play</servlet-name>
+    <servlet-class>play.core.server.servlet25.Play2Servlet</servlet-class>
+  </servlet>
+  <servlet-mapping>
+    <servlet-name>play</servlet-name>
+    <url-pattern>/</url-pattern>
+  </servlet-mapping>
+</web-app>
+""")
+	}
+  }
+
   lazy val saeSettings = Seq(
 	libraryDependencies += "org.codeck.play2sae" %% "play2-wrapper" % wrapperVer excludeAll (
 	  ExclusionRule(organization = "com.typesafe.play"))
@@ -50,7 +76,7 @@ object sbtPlugin extends Plugin
 	warPostProcess in Compile := {
 	  () =>
 	  val webapp = target.value / "webapp"
-	  IO.delete(webapp / "WEB-INF" / "lib" /"servlet-api-2.5.jar")
+	  warMassage(webapp)
 	  IO.listFiles(webapp / "WEB-INF" / "lib", saeLibFilter).map(
 		l => IO.delete(l)
 	  )
@@ -65,7 +91,7 @@ object sbtPlugin extends Plugin
 	warPostProcess in Compile := {
 	  () =>
 	  val webapp = target.value / "webapp"
-	  IO.delete(webapp / "WEB-INF" / "lib" /"servlet-api-2.5.jar")
+	  warMassage(webapp)
 	}
   )
 
