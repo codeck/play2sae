07914cf7c3931572b9a4c09d243e1ae58ab1980b
Cheng Kuan <ck@codeck.org>
2013-10-18 09:45:11 +0800

    Update 1.sql
    copy from https://github.com/sscarano/computer-database-mysql/edit/master/conf/evolutions/default/1.sql

--- samples/computer-database/conf/evolutions/default/1.sql
+++ samples/computer-database/conf/evolutions/default/1.sql
@@ -2,26 +2,21 @@
 
 # --- !Ups
 
-set ignorecase true;
-
 create table company (
-  id                        bigint not null,
+  id                        bigint not null auto_increment,
   name                      varchar(255) not null,
   constraint pk_company primary key (id))
 ;
 
 create table computer (
-  id                        bigint not null,
+  id                        bigint not null auto_increment,
   name                      varchar(255) not null,
-  introduced                timestamp,
-  discontinued              timestamp,
+  introduced                datetime null,
+  discontinued              datetime null,
   company_id                bigint,
   constraint pk_computer primary key (id))
 ;
 
-create sequence company_seq start with 1000;
-
-create sequence computer_seq start with 1000;
 
 alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
 create index ix_computer_company_1 on computer (company_id);
@@ -29,15 +24,11 @@ create index ix_computer_company_1 on computer (company_id);
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS = 0
 
 drop table if exists company;
 
 drop table if exists computer;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists company_seq;
-
-drop sequence if exists computer_seq;
+SET FOREIGN_KEY_CHECKS = 1
 
